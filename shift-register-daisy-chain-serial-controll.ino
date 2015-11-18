74982f36034e1c5b122434002711a1f12c491074
Nick Chkhikvishvili <frrrredo@gmail.com>
2015-11-18 21:40:17 +0400

    Update shift-register-daisy-chain-serial-controll.ino

--- shift-register-daisy-chain-serial-controll.ino
+++ shift-register-daisy-chain-serial-controll.ino
@@ -145,8 +145,7 @@ void relaycontroll()
 
 
 
-
-/////////////////////////// Relay 1 
+/////////////////////////// Relay 1
      if (input == "dev1_on")
      {
       setRegisterPin(dev1, HIGH);
@@ -163,7 +162,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 1
-/////////////////////////// Relay 2 
+/////////////////////////// Relay 2
      if (input == "dev2_on")
      {
       setRegisterPin(dev2, HIGH);
@@ -180,7 +179,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 2
-/////////////////////////// Relay 3 
+/////////////////////////// Relay 3
      if (input == "dev3_on")
      {
       setRegisterPin(dev3, HIGH);
@@ -197,7 +196,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 3
-/////////////////////////// Relay 4 
+/////////////////////////// Relay 4
      if (input == "dev4_on")
      {
       setRegisterPin(dev4, HIGH);
@@ -214,7 +213,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 4
-/////////////////////////// Relay 5 
+/////////////////////////// Relay 5
      if (input == "dev5_on")
      {
       setRegisterPin(dev5, HIGH);
@@ -231,7 +230,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 5
-/////////////////////////// Relay 6 
+/////////////////////////// Relay 6
      if (input == "dev6_on")
      {
       setRegisterPin(dev6, HIGH);
@@ -248,7 +247,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 6
-/////////////////////////// Relay 7 
+/////////////////////////// Relay 7
      if (input == "dev7_on")
      {
       setRegisterPin(dev7, HIGH);
@@ -265,7 +264,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 7
-/////////////////////////// Relay 8 
+/////////////////////////// Relay 8
      if (input == "dev8_on")
      {
       setRegisterPin(dev8, HIGH);
@@ -282,7 +281,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 8
-/////////////////////////// Relay 9 
+/////////////////////////// Relay 9
      if (input == "dev9_on")
      {
       setRegisterPin(dev9, HIGH);
@@ -299,7 +298,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 9
-/////////////////////////// Relay 10 
+/////////////////////////// Relay 10
      if (input == "dev10_on")
      {
       setRegisterPin(dev10, HIGH);
@@ -316,7 +315,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 10
-/////////////////////////// Relay 11 
+/////////////////////////// Relay 11
      if (input == "dev11_on")
      {
       setRegisterPin(dev11, HIGH);
@@ -333,7 +332,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 11
-/////////////////////////// Relay 12 
+/////////////////////////// Relay 12
      if (input == "dev12_on")
      {
       setRegisterPin(dev12, HIGH);
@@ -350,7 +349,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 12
-/////////////////////////// Relay 13 
+/////////////////////////// Relay 13
      if (input == "dev13_on")
      {
       setRegisterPin(dev13, HIGH);
@@ -367,7 +366,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 13
-/////////////////////////// Relay 14 
+/////////////////////////// Relay 14
      if (input == "dev14_on")
      {
       setRegisterPin(dev14, HIGH);
@@ -384,7 +383,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 14
-/////////////////////////// Relay 15 
+/////////////////////////// Relay 15
      if (input == "dev15_on")
      {
       setRegisterPin(dev15, HIGH);
@@ -401,7 +400,7 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 15
-/////////////////////////// Relay 16 
+/////////////////////////// Relay 16
      if (input == "dev16_on")
      {
       setRegisterPin(dev16, HIGH);
@@ -418,7 +417,550 @@ void relaycontroll()
   
 }
 ////////////////////////// end Relay 16
-
+/////////////////////////// Relay 17
+     if (input == "dev17_on")
+     {
+      setRegisterPin(dev17, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev17_off") 
+{ 
+      setRegisterPin(dev17, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 17
+/////////////////////////// Relay 18
+     if (input == "dev18_on")
+     {
+      setRegisterPin(dev18, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev18_off") 
+{ 
+      setRegisterPin(dev18, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 18
+/////////////////////////// Relay 19
+     if (input == "dev19_on")
+     {
+      setRegisterPin(dev19, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev19_off") 
+{ 
+      setRegisterPin(dev19, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 19
+/////////////////////////// Relay 20
+     if (input == "dev20_on")
+     {
+      setRegisterPin(dev20, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev20_off") 
+{ 
+      setRegisterPin(dev20, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 20
+/////////////////////////// Relay 21
+     if (input == "dev21_on")
+     {
+      setRegisterPin(dev21, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev21_off") 
+{ 
+      setRegisterPin(dev21, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 21
+/////////////////////////// Relay 22
+     if (input == "dev22_on")
+     {
+      setRegisterPin(dev22, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev22_off") 
+{ 
+      setRegisterPin(dev22, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 22
+/////////////////////////// Relay 23
+     if (input == "dev23_on")
+     {
+      setRegisterPin(dev23, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev23_off") 
+{ 
+      setRegisterPin(dev23, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 23
+/////////////////////////// Relay 24
+     if (input == "dev24_on")
+     {
+      setRegisterPin(dev24, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev24_off") 
+{ 
+      setRegisterPin(dev24, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 24
+/////////////////////////// Relay 25
+     if (input == "dev25_on")
+     {
+      setRegisterPin(dev25, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev25_off") 
+{ 
+      setRegisterPin(dev25, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 25
+/////////////////////////// Relay 26
+     if (input == "dev26_on")
+     {
+      setRegisterPin(dev26, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev26_off") 
+{ 
+      setRegisterPin(dev26, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 26
+/////////////////////////// Relay 27
+     if (input == "dev27_on")
+     {
+      setRegisterPin(dev27, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev27_off") 
+{ 
+      setRegisterPin(dev27, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 27
+/////////////////////////// Relay 28
+     if (input == "dev28_on")
+     {
+      setRegisterPin(dev28, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev28_off") 
+{ 
+      setRegisterPin(dev28, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 28
+/////////////////////////// Relay 29
+     if (input == "dev29_on")
+     {
+      setRegisterPin(dev29, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev29_off") 
+{ 
+      setRegisterPin(dev29, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 29
+/////////////////////////// Relay 30
+     if (input == "dev30_on")
+     {
+      setRegisterPin(dev30, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev30_off") 
+{ 
+      setRegisterPin(dev30, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 30
+/////////////////////////// Relay 31
+     if (input == "dev31_on")
+     {
+      setRegisterPin(dev31, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev31_off") 
+{ 
+      setRegisterPin(dev31, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 31
+/////////////////////////// Relay 32
+     if (input == "dev32_on")
+     {
+      setRegisterPin(dev32, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev32_off") 
+{ 
+      setRegisterPin(dev32, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 32
+/////////////////////////// Relay 33
+     if (input == "dev33_on")
+     {
+      setRegisterPin(dev33, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev33_off") 
+{ 
+      setRegisterPin(dev33, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 33
+/////////////////////////// Relay 34
+     if (input == "dev34_on")
+     {
+      setRegisterPin(dev34, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev34_off") 
+{ 
+      setRegisterPin(dev34, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 34
+/////////////////////////// Relay 35
+     if (input == "dev35_on")
+     {
+      setRegisterPin(dev35, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev35_off") 
+{ 
+      setRegisterPin(dev35, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 35
+/////////////////////////// Relay 36
+     if (input == "dev36_on")
+     {
+      setRegisterPin(dev36, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev36_off") 
+{ 
+      setRegisterPin(dev36, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 36
+/////////////////////////// Relay 37
+     if (input == "dev37_on")
+     {
+      setRegisterPin(dev37, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev37_off") 
+{ 
+      setRegisterPin(dev37, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 37
+/////////////////////////// Relay 38
+     if (input == "dev38_on")
+     {
+      setRegisterPin(dev38, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev38_off") 
+{ 
+      setRegisterPin(dev38, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 38
+/////////////////////////// Relay 39
+     if (input == "dev39_on")
+     {
+      setRegisterPin(dev39, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev39_off") 
+{ 
+      setRegisterPin(dev39, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 39
+/////////////////////////// Relay 40
+     if (input == "dev40_on")
+     {
+      setRegisterPin(dev40, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev40_off") 
+{ 
+      setRegisterPin(dev40, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 40
+/////////////////////////// Relay 41
+     if (input == "dev41_on")
+     {
+      setRegisterPin(dev41, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev41_off") 
+{ 
+      setRegisterPin(dev41, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 41
+/////////////////////////// Relay 42
+     if (input == "dev42_on")
+     {
+      setRegisterPin(dev42, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev42_off") 
+{ 
+      setRegisterPin(dev42, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 42
+/////////////////////////// Relay 43
+     if (input == "dev43_on")
+     {
+      setRegisterPin(dev43, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev43_off") 
+{ 
+      setRegisterPin(dev43, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 43
+/////////////////////////// Relay 44
+     if (input == "dev44_on")
+     {
+      setRegisterPin(dev44, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev44_off") 
+{ 
+      setRegisterPin(dev44, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 44
+/////////////////////////// Relay 45
+     if (input == "dev45_on")
+     {
+      setRegisterPin(dev45, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev45_off") 
+{ 
+      setRegisterPin(dev45, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 45
+/////////////////////////// Relay 46
+     if (input == "dev46_on")
+     {
+      setRegisterPin(dev46, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev46_off") 
+{ 
+      setRegisterPin(dev46, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 46
+/////////////////////////// Relay 47
+     if (input == "dev47_on")
+     {
+      setRegisterPin(dev47, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev47_off") 
+{ 
+      setRegisterPin(dev47, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 47
+/////////////////////////// Relay 48
+     if (input == "dev48_on")
+     {
+      setRegisterPin(dev48, HIGH);
+      writeRegisters();
+      delay(delayTime);
+   //   clearRegisters();
+     }
+     else if(input == "dev48_off") 
+{ 
+      setRegisterPin(dev48, LOW);
+      writeRegisters();
+      delay(delayTime);
+  //    clearRegisters();
+  
+}
+////////////////////////// end Relay 48
 
 
 
