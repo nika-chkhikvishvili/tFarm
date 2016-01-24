0eef3f2974dee42f9e52a40390cc876b1f388b69
Nick Chkhikvishvili <frrrredo@gmail.com>
2016-01-24 13:14:22 +0400

    Update eth2relay.ino

--- eth2relay.ino
+++ eth2relay.ino
@@ -51,6 +51,22 @@ int dev14=13;
 int dev15=14;
 int dev16=15;
 int dev17=16;
+int dev18=17;
+int dev19=18;
+int dev20=19;
+int dev21=20;
+int dev22=21;
+int dev23=22;
+int dev24=23;
+int dev25=24;
+int dev26=25;
+int dev27=26;
+int dev28=27;
+int dev29=28;
+int dev30=29;
+int dev31=30;
+int dev32=31;
+
 ////////////////////////////////////////////////////////
 
 
@@ -147,39 +163,247 @@ void checkForClient(){
           Serial.print(c);
 
            switch (c) {
-            case 'A':
-              //add code here to trigger on 2
+
+
+  case 'A':
+             //add code here to trigger on 2
               triggerPin(dev1, client);
               break;
-            case 'a':
+          case 'a':
             //add code here to trigger on 3
               triggerOff(dev1, client);
               break;
-            case 'B':
-            //add code here to trigger on 4
+
+  case 'B':
+             //add code here to trigger on 2
               triggerPin(dev2, client);
               break;
-            case 'b':
-            //add code here to trigger on 5
+          case 'b':
+            //add code here to trigger on 3
               triggerOff(dev2, client);
               break;
-            case 'C':
-            //add code here to trigger on 6
+
+  case 'C':
+             //add code here to trigger on 2
               triggerPin(dev3, client);
               break;
-            case 'c':
-            //add code here to trigger on 7
+          case 'c':
+            //add code here to trigger on 3
               triggerOff(dev3, client);
               break;
-            case 'D':
-            //add code here to trigger on 8
+
+  case 'D':
+             //add code here to trigger on 2
               triggerPin(dev4, client);
               break;
-            case 'd':
-            //add code here to trigger on 9
+          case 'd':
+            //add code here to trigger on 3
               triggerOff(dev4, client);
               break;
-          }
+
+  case 'E':
+             //add code here to trigger on 2
+              triggerPin(dev5, client);
+              break;
+          case 'e':
+            //add code here to trigger on 3
+              triggerOff(dev5, client);
+              break;
+
+  case 'F':
+             //add code here to trigger on 2
+              triggerPin(dev6, client);
+              break;
+          case 'f':
+            //add code here to trigger on 3
+              triggerOff(dev6, client);
+              break;
+
+  case 'G':
+             //add code here to trigger on 2
+              triggerPin(dev7, client);
+              break;
+          case 'g':
+            //add code here to trigger on 3
+              triggerOff(dev7, client);
+              break;
+
+  case 'H':
+             //add code here to trigger on 2
+              triggerPin(dev8, client);
+              break;
+          case 'h':
+            //add code here to trigger on 3
+              triggerOff(dev8, client);
+              break;
+
+  case 'I':
+             //add code here to trigger on 2
+              triggerPin(dev9, client);
+              break;
+          case 'i':
+            //add code here to trigger on 3
+              triggerOff(dev9, client);
+              break;
+
+  case 'J':
+             //add code here to trigger on 2
+              triggerPin(dev10, client);
+              break;
+          case 'j':
+            //add code here to trigger on 3
+              triggerOff(dev10, client);
+              break;
+
+  case 'K':
+             //add code here to trigger on 2
+              triggerPin(dev11, client);
+              break;
+          case 'k':
+            //add code here to trigger on 3
+              triggerOff(dev11, client);
+              break;
+
+  case 'L':
+             //add code here to trigger on 2
+              triggerPin(dev12, client);
+              break;
+          case 'l':
+            //add code here to trigger on 3
+              triggerOff(dev12, client);
+              break;
+
+  case 'M':
+             //add code here to trigger on 2
+              triggerPin(dev13, client);
+              break;
+          case 'm':
+            //add code here to trigger on 3
+              triggerOff(dev13, client);
+              break;
+
+  case 'N':
+             //add code here to trigger on 2
+              triggerPin(dev14, client);
+              break;
+          case 'n':
+            //add code here to trigger on 3
+              triggerOff(dev14, client);
+              break;
+
+  case 'O':
+             //add code here to trigger on 2
+              triggerPin(dev15, client);
+              break;
+          case 'o':
+            //add code here to trigger on 3
+              triggerOff(dev15, client);
+              break;
+
+  case 'P':
+             //add code here to trigger on 2
+              triggerPin(dev16, client);
+              break;
+          case 'p':
+            //add code here to trigger on 3
+              triggerOff(dev16, client);
+              break;
+
+  case 'Q':
+             //add code here to trigger on 2
+              triggerPin(dev17, client);
+              break;
+          case 'q':
+            //add code here to trigger on 3
+              triggerOff(dev17, client);
+              break;
+
+  case 'R':
+             //add code here to trigger on 2
+              triggerPin(dev18, client);
+              break;
+          case 'r':
+            //add code here to trigger on 3
+              triggerOff(dev18, client);
+              break;
+
+  case 'S':
+             //add code here to trigger on 2
+              triggerPin(dev19, client);
+              break;
+          case 's':
+            //add code here to trigger on 3
+              triggerOff(dev19, client);
+              break;
+
+  case 'T':
+             //add code here to trigger on 2
+              triggerPin(dev20, client);
+              break;
+          case 't':
+            //add code here to trigger on 3
+              triggerOff(dev20, client);
+              break;
+
+  case 'U':
+             //add code here to trigger on 2
+              triggerPin(dev21, client);
+              break;
+          case 'u':
+            //add code here to trigger on 3
+              triggerOff(dev21, client);
+              break;
+
+  case 'V':
+             //add code here to trigger on 2
+              triggerPin(dev22, client);
+              break;
+          case 'v':
+            //add code here to trigger on 3
+              triggerOff(dev22, client);
+              break;
+
+  case 'W':
+             //add code here to trigger on 2
+              triggerPin(dev23, client);
+              break;
+          case 'w':
+            //add code here to trigger on 3
+              triggerOff(dev23, client);
+              break;
+
+  case 'X':
+             //add code here to trigger on 2
+              triggerPin(dev24, client);
+              break;
+          case 'x':
+            //add code here to trigger on 3
+              triggerOff(dev24, client);
+              break;
+
+  case 'Y':
+             //add code here to trigger on 2
+              triggerPin(dev25, client);
+              break;
+          case 'y':
+            //add code here to trigger on 3
+              triggerOff(dev25, client);
+              break;
+
+  case 'Z':
+             //add code here to trigger on 2
+              triggerPin(dev26, client);
+              break;
+          case 'z':
+            //add code here to trigger on 3
+              triggerOff(dev26, client);
+              break;
+
+
+      
+      
+      
+           }
 
         }
 
